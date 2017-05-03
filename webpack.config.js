9414902ca20e9adcd953221b330eaba96bd704a0
lexamo <amobe.momoko@gmail.com>
2017-05-03 17:49:07 +0800

    Add index with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './src/main',
+        'webpack-dev-server/client?http://localhost:8080'
+    ],
+    output: {
+        publicPath: '/',
+        filename: 'main.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+                // Skip any files outside of your project's `src` directory
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                // Only run `.js` file through Babel
+                test: /\.js$/,
+                // Options to configure babel with
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-0'],
+                }
+            },
+        ]
+    },
+    devServer: {
+        contentBase: './src'
+    }
+}
+
