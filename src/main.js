1921b5341ea804da010faf85a9860412b573b9fb
lexamo <amobe.momoko@gmail.com>
2017-05-04 16:55:49 +0800

    Add PIXI canvas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 'use strict';
 
-var PIXI = require('pixi.js')
-console.log(PIXI)
+var PIXI = require('pixi.js');
+
+var renderer = PIXI.autoDetectRenderer(1000, 1000);
+renderer.backgroundColor = 0x061639;
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+renderer.render(stage);
